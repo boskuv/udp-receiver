6ef1978c92751927626d2ff660dd609f5694b63c
hjklawer <hjklawer@mail.ru>
2023-09-03 19:08:49 +0300

    refactor: move PacketConn init to method

--- main.go
+++ main.go
@@ -24,52 +24,77 @@ func init() {
 
 func main() {
 
-	pc1, err := net.ListenPacket("udp4", ":8829")
+	// pc1, err := net.ListenPacket("udp4", ":8829")
 
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// pc2, err := net.ListenPacket("udp4", ":8830")
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// defer pc1.Close()
+	// defer pc2.Close()
+
+	// go handlePacket(pc1, "")
+	// go handlePacket(pc2, "")
+	ports := []int{8829, 8830} // add more ports here
+	conns, err := listenOnPorts(ports)
 	if err != nil {
 		panic(err)
 	}
-
-	pc2, err := net.ListenPacket("udp4", ":8830")
-
-	if err != nil {
-		panic(err)
+	defer func() {
+		for _, conn := range conns {
+			conn.Close()
+		}
+	}()
+	for i, conn := range conns {
+		go handlePacket(conn, fmt.Sprintf("Service %d", i))
 	}
 
-	defer pc1.Close()
-	defer pc2.Close()
-
-	go handlePacket(pc1, "")
-	go handlePacket(pc2, "")
-
-	select {}
+	select {} // TODO: what for?
 }
 
-func handlePacket(pc net.PacketConn, port string) {
-	buf := make([]byte, 1024)
+func listenOnPorts(ports []int) ([]net.PacketConn, error) {
+	var conns []net.PacketConn
+	for _, port := range ports {
+		addr := fmt.Sprintf(":%d", port)
+		conn, err := net.ListenPacket("udp4", addr)
+		if err != nil {
+			return nil, err
+		}
+		conns = append(conns, conn) // TODO: slice?
+	}
+	return conns, nil
+}
 
-	lastReceived := time.Now()
+func handlePacket(pc net.PacketConn, serviceName string) {
+	buf := make([]byte, 1024) // TODO: check size
+
+	//lastReceived := time.Now()
+	wasPacketReceived := false
 
 	for {
 		pc.SetReadDeadline(time.Now().Add(5 * time.Second)) // TODO: cfg parameter
-		n, addr, err := pc.ReadFrom(buf)
-		if err != nil {
-			fmt.Println("Error reading UDP packet:", err)
-			continue
+		n, _, err := pc.ReadFrom(buf)
+		if err == nil {
+			fmt.Printf("UDP packet was received: %s\n", buf[:n])
+			wasPacketReceived = true
 		}
 
-		Prom(lastReceived, port)
+		Prom(wasPacketReceived, serviceName)
 
-		fmt.Printf("%s sent this to port %s: %s\n", addr, port, buf[:n])
-		lastReceived = time.Now()
 	}
 }
 
-func Prom(lastReceived time.Time, port string) {
-	if time.Since(lastReceived) > time.Minute {
-		caster_status.WithLabelValues(port).Set(0)
+func Prom(wasPacketReceived bool, serviceName string) {
+	if wasPacketReceived {
+		caster_status.WithLabelValues(serviceName).Set(1)
 	} else {
-		caster_status.WithLabelValues(port).Set(1)
+		caster_status.WithLabelValues(serviceName).Set(0)
 	}
-	time.Sleep(time.Second)
+	time.Sleep(time.Second * 5)
 }
