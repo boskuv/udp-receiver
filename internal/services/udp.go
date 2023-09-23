6bd65803a150ea60d41afc67aa1db4d768f6cbf4
boskuv <boris_kv@bk.ru>
2023-09-23 22:05:27 +0300

    fix: add working ability to reuse port

--- internal/services/udp.go
+++ internal/services/udp.go
@@ -3,47 +3,32 @@ package services
 import (
 	"fmt"
 	"net"
-	"syscall"
 	"time"
+
+	"github.com/libp2p/go-reuseport"
 )
 
 type ServiceNetConnection struct {
 	serviceName string
-	PacketConn  net.UDPConn
+	PacketConn  net.PacketConn
 }
 
 func ListenOnPorts(udpServices map[string]int, AnswerTimeoutSec int) (*[]ServiceNetConnection, error) {
 	var serviceConns []ServiceNetConnection
 	for serviceName, port := range udpServices {
 
-		addr := &net.UDPAddr{
-			IP:   net.ParseIP("0.0.0.0"),
-			Port: port,
-		}
+		addr := fmt.Sprintf("0.0.0.0:%d", port)
 
-		conn, err := net.ListenUDP("udp", addr)
+		conn, err := reuseport.ListenPacket("udp4", addr)
 		if err != nil {
-			return nil, fmt.Errorf("ListenOnPorts(..)->net.ListenUDP(..) to service %s: %w", serviceName, err)
+			return nil, fmt.Errorf("ListenOnPorts(..)->reuseport.ListenPacket(..) to service %s: %w", serviceName, err)
 		}
-
-		fd, err := conn.File()
-		if err != nil {
-			return nil, fmt.Errorf("ListenOnPorts(..) Error getting file descriptor in service %s: %w", serviceName, err)
-		}
-
-		err = syscall.SetsockoptInt(int(fd.Fd()), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
-		if err != nil {
-			return nil, fmt.Errorf("ListenOnPorts(..) Error setting SO_REUSEPORT option in service %s: %w", serviceName, err)
-		}
-		fd.Close()
-
-		defer conn.Close()
 
 		conn.SetReadDeadline(time.Now().Add(time.Duration(AnswerTimeoutSec) * time.Second))
 
 		serviceConns = append(serviceConns, ServiceNetConnection{
 			serviceName: serviceName,
-			PacketConn:  *conn,
+			PacketConn:  conn,
 		})
 	}
 	return &serviceConns, nil
@@ -60,7 +45,7 @@ func HandlePacket(SleepTimeSec int, serviceConns *[]ServiceNetConnection, status
 			var serviceStatus float64
 
 			for {
-				_, _, err := conn.PacketConn.ReadFromUDP(dataBuffer)
+				_, _, err := conn.PacketConn.ReadFrom(dataBuffer)
 
 				serviceStatus = 0
 
